bc4d5b1bd3c2c98b4faba05446e87a4b1bd759fd
hwen <whman63@gmail.com>
2020-05-05 20:28:06 +0800

    initial commit

---
+++ web2020apr_p01_assignment_group5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace web2020apr_p01_assignment_group5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
