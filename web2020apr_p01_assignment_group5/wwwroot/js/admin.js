4964c1ed882659daeb83187ce74834ee64396799
hwen <whman63@gmail.com>
2020-06-16 17:38:44 +0800

    change button css style when toggle

--- web2020apr_p01_assignment_group5/wwwroot/js/admin.js
+++ web2020apr_p01_assignment_group5/wwwroot/js/admin.js
@@ -1,10 +1,16 @@
 function toggle(i) {
     var tablename = "flights_table" + i;
-    console.log(tablename);
+    var arrowname = "arrow" + i;
     var x = document.getElementById(tablename);
+    var arrow = document.getElementById(arrowname);
     if (x.style.display === "none") {
         x.style.display = "block";
+        arrow.style["border-bottom"] = "16px solid black";
+        arrow.style["border-top"] = "0px";
+
     } else {
         x.style.display = "none";
+        arrow.style["border-bottom"] = "0px";
+        arrow.style["border-top"] = "16px solid black";
     }
 }
